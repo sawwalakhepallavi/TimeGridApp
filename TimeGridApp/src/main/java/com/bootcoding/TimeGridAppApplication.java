6ba417aa2b18e9fd91b72a11eed3a2a0c5766af5
sawwalakhepallavi <sawwalakhepallavi@gmail.com>
2023-08-23 15:21:42 +0530

    Initial commit

---
+++ TimeGridApp/src/main/java/com/bootcoding/TimeGridAppApplication.java
@@ -0,0 +1,13 @@
+package com.bootcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeGridAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeGridAppApplication.class, args);
+	}
+
+}
